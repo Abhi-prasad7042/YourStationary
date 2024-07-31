1fd7f19f904330de5da663c8f5f56ccc064927f6
Abhi-prasad7042 <abhiprasad7042@gmail.com>
2024-07-31 13:33:56 +0530

    optimized cards

--- src/components/StationeryList.jsx
+++ src/components/StationeryList.jsx
@@ -3,9 +3,9 @@ import { DATA } from '../utils/constant';
 
 const StationeryList = () => {
   return (
-    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 px-5 sm:px-20 p-4 mt-16">
+    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 sm:px-20 p-4 mt-16">
       {DATA.map((item, index) => (
-        <div key={index} className="border p-4 rounded-lg shadow-xl w-72 h-92 flex flex-col">
+        <div key={index} className="border p-4 rounded-lg shadow-xl flex flex-col items-center">
           <div className="relative w-full h-40 mb-4">
             <img
               src={item.img}
@@ -14,12 +14,12 @@ const StationeryList = () => {
               style={{ objectFit: 'cover' }} // Ensures images cover the container
             />
           </div>
-          <h2 className="text-xl font-bold">{item.name}</h2>
-          <p className="text-gray-700">{item.description}</p>
-          <p className="text-gray-900 font-bold mb-2">${item.price.toFixed(2)}</p>
-          <div className='flex items-center justify-between'>
-            <p className="text-yellow-500">Rating: {item.rating} / 5</p>
-            <button className='bg-green-400 text-white px-2 py-1 rounded-lg hover:bg-green-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50 transition duration-300'>Add to Cart</button>
+          <h2 className="text-xl font-bold text-center">{item.name}</h2>
+          <p className="text-gray-700 text-center">{item.description}</p>
+          <p className="text-gray-900 font-bold mb-2 text-center">${item.price.toFixed(2)}</p>
+          <div className='flex items-center justify-between w-full'>
+            <p className="text-yellow-500 text-center">Rating: {item.rating} / 5</p>
+            <button className='bg-green-400 text-white px-4 py-2 rounded-lg hover:bg-green-600 focus:outline-none focus:ring-2 focus:ring-green-500 focus:ring-opacity-50 transition duration-300'>Add to Cart</button>
           </div>
         </div>
       ))}
